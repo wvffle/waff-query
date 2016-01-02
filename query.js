d32b1943f7899b89465532f6c9ecbaa4fafd2459
Casper Seweryn <casper@wvffle.net>
2016-01-03 00:57:54 +0100

    Create elements with class

--- query.js
+++ query.js
@@ -12,7 +12,12 @@ var qq = function(qs, root){
 var q = function(qs, root){
   return qq(qs, root)[0]||null;
 }
-
+//-- Create elements
+var c = function(_class){
+  var e = document.createElement('div');
+  e.classList.add(_class);
+  return e;
+}
 // -- Query inside element
 Element.prototype.qq = function(qs){
   return qq(qs, this);
@@ -20,7 +25,12 @@ Element.prototype.qq = function(qs){
 Element.prototype.q = function(qs){
   return q(qs, this);
 }
-
+// -- Create elements inside elements
+Element.prototype.c = function (_class) {
+  var e = c(_class);
+  this.appendChild(e);
+  return e;
+};
 // -- Binding events
 Element.prototype.on = function(event, next){
   var self = this;
